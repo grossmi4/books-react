2af64c427cc3cc115f6d9b99a285b26b4426086c
Mike Gross <grossmi4@gmail.com>
2019-01-17 10:50:43 -0500

    minor bug

--- client/src/pages/SavedPage.js
+++ client/src/pages/SavedPage.js
@@ -25,7 +25,7 @@ class SavedPage extends React.Component {
       id: book._id
     };
     API.deleteBook(deletedBook.id)
-      .then(API.getSavedBooks())
+      .then(API.getSavedBooks()
         .then(res => {
           this.setState({
             books: res.data
